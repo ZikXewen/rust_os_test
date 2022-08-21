9726b99bbe21fd93669f5d6f3b67225afcd3c144
ZikXewen <datasup12@gmail.com>
2022-08-22 03:13:57 +0700

    refactor should_panic test

--- tests/should_panic.rs
+++ tests/should_panic.rs
@@ -1,8 +1,5 @@
 #![no_std]
 #![no_main]
-#![feature(custom_test_frameworks)]
-#![test_runner(test_runner)]
-#![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
 use rust_os_test::{exit_qemu, serial_print, serial_println, QemuExitCode};
@@ -16,19 +13,12 @@ fn panic(_info: &PanicInfo) -> ! {
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    test_main();
+    should_fail();
+    serial_println!("[did not panic]");
+    exit_qemu(QemuExitCode::Failed);
     loop {}
 }
 
-pub fn test_runner(tests: &[&dyn Fn()]) {
-    if let Some(test) = tests.first() {
-        test();
-        serial_println!("[did not panic]");
-        exit_qemu(QemuExitCode::Failed);
-    }
-}
-
-#[test_case]
 fn should_fail() {
     serial_print!("Running 1 tests...\nshould_panic::should_fail...\t");
     assert_eq!(0, 1);
